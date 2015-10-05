23a869b4bf3c14ca0834ce512bffa4fe6d886384
timothee <tim.dorand@gmail.com>
2015-10-05 18:15:43 +0200

    Refix #2.

--- validation.php
+++ validation.php
@@ -7,6 +7,27 @@ if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['passwor
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
 	// TODO
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+
+	if(isEmailAvailable($db, $email) === true ){
+		if(isUsernameAvailable($db, $username) === true){
+
+				userRegistration($db, $username, $email, $password);
+				$_SESSION['message'] = 'Bien joué';
+				header('Location:login.php');
+
+}else{
+			$_SESSION['message'] = 'Mauvais email';
+			header('Location: register.php');
+
+			}
+		}else{
+		$_SESSION['message'] = 'Mauvais pseudo';
+		header('Location: register.php');
+}
+	
 
 }else{ 
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
